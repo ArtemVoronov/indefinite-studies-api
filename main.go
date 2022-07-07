48e9df7e9d269e15063cd6b0aa67d90732285389
voronov <voronov54@gmail.com>
2022-07-07 18:44:18 +0700

    added authorization and graceful shutdown of server

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/ArtemVoronov/indefinite-studies-api/api/rest/v1/ping"
 	"github.com/ArtemVoronov/indefinite-studies-api/api/rest/v1/tasks"
@@ -13,35 +18,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func Cors() gin.HandlerFunc {
-	// TODO: for release add appropiate domains
-	return func(c *gin.Context) {
-		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
-		c.Next()
-	}
-}
-
-// TODO: add authorization
-// TODO: add graceful shutdown
-
 func main() {
 
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-
-	port, portExists := os.LookupEnv("APP_PORT")
-	if !portExists {
-		port = "3000"
-	}
-
-	host := ":" + port
-
-	log.Println("starting indefinite-studies-api at " + host + " ...")
+	initEnv()
+	apiUsers := getApiUsers()
+	host := getHost()
 
 	router := gin.Default()
 
-	router.Use(Cors())
+	router.Use(cors())
 
 	// Global middleware
 	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
@@ -58,7 +43,7 @@ func main() {
 
 	db.DB = db.Setup()
 
-	v1 := router.Group("/api/v1")
+	v1 := router.Group("/api/v1", gin.BasicAuth(apiUsers))
 	{
 		v1.GET("/ping", ping.Ping)
 		v1.GET("/tasks/", tasks.GetTasks)
@@ -68,6 +53,76 @@ func main() {
 		v1.DELETE("/tasks/:id", tasks.DeleteTask)
 	}
 
+	startServer(host, router)
+}
+
+func cors() gin.HandlerFunc {
+	// TODO: for release add appropiate domains
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+
+func initEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func getHost() string {
+	port, portExists := os.LookupEnv("APP_PORT")
+	if !portExists {
+		port = "3000"
+	}
+	host := ":" + port
+	return host
+}
+
+func getApiUsers() gin.Accounts {
+	apiKey, apiKeyExists := os.LookupEnv("AUTH_USERNAME")
+	if !apiKeyExists {
+		log.Fatalf("Missed enviroment variable: %s. Check the .env file or OS enviroment vars", "AUTH_USERNAME")
+	}
+
+	apiAuthUser, apiAuthUserExists := os.LookupEnv("AUTH_PASSWORD")
+	if !apiAuthUserExists {
+		log.Fatalf("Missed enviroment variable: %s. Check the .env file or OS enviroment vars", "AUTH_PASSWORD")
+	}
+	apiUsers := gin.Accounts{apiKey: apiAuthUser}
+	return apiUsers
+}
+
+func startServer(host string, router *gin.Engine) {
+	srv := &http.Server{
+		Addr:    host,
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
 	defer db.DB.Close()
-	router.Run(host)
+	log.Println("Server exiting")
 }
